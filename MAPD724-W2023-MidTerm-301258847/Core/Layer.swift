528ee3b68bb64f485e1de1ca25b05c58bc450f46
PolamW <pwong140@my.centennialcollege.ca>
2023-03-02 16:06:35 -0500

    added comments

--- MAPD724-W2023-MidTerm-301258847/Core/Layer.swift
+++ MAPD724-W2023-MidTerm-301258847/Core/Layer.swift
@@ -1,6 +1,9 @@
 //
 //  Layer.swift
 //  MAPD724-W2023-MidTerm-301258847
+//  Name: Po Lam Wong, Lizolet
+//  Student ID: 301258847
+//  Description: Mail Pilot - Landscape version
 //
 //  Created by Po Lam Wong on 28/2/2023.
 //
--- MAPD724-W2023-MidTerm-301258847/GameObjects/Landscape/CloudLandscape.swift
+++ MAPD724-W2023-MidTerm-301258847/GameObjects/Landscape/CloudLandscape.swift
@@ -1,9 +1,13 @@
 //
 //  Cloud.swift
 //  MAPD724-W2023-MidTerm-301258847
+//  Name: Po Lam Wong, Lizolet
+//  Student ID: 301258847
+//  Description: Mail Pilot - Landscape version
 //
 //  Created by Po Lam Wong on 28/2/2023.
 //
+
 
 import GameplayKit
 import SpriteKit
