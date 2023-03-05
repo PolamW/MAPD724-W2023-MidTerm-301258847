0b74faeab9d984c20e821d367009576db80d79cd
PolamW <pwong140@my.centennialcollege.ca>
2023-03-05 17:05:13 -0500

    Edited comments

--- MAPD724-W2023-MidTerm-301258847/Scenes/EndScene.swift
+++ MAPD724-W2023-MidTerm-301258847/Scenes/EndScene.swift
@@ -2,7 +2,7 @@
 //  MAPD724-W2023-MidTerm-301258847
 //  Name: Po Lam Wong, Lizolet
 //  Student ID: 301258847
-//  Description: Mail Pilot - Landscape version
+//  Description: Modify Mail Pilot
 //
 //  Created by Po Lam Wong on 28/2/2023.
 //
